216c779f3d26dddfd71f9dad864a0924f8f40656
Satwik <96126696+satwiksatwik69@users.noreply.github.com>
2022-02-06 17:12:28 +0530

    Delete ML Week 1 task 1.2.py

--- ML Week 1 task 1.2.py
+++
@@ -1,9 +0,0 @@
-mat1 = [[6,2,6],[9,6,9],[7,9,6],[1,2,9]]
-print("Original matrix:\n")
-for row in mat1 :
-    print(row)
-mat11 = [[mat1[j][i] for j in range(len(mat1))] for i in range(len(mat1[0]))]
-print("\n\n")
-print("Transpose Matrix:\n")
-for row in mat11:
-    print(row)
