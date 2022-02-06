2a098c8789fa07ebf5f90bf9be65527152ce1b58
rakesh9541 <86745045+rakesh9541@users.noreply.github.com>
2022-02-06 22:37:05 +0530

    Create RemTup.py
    done

---
+++ Week1/RemTup.py
@@ -0,0 +1,7 @@
+def emptytuples(List):
+    List=filter(None,List)
+    return List
+
+List=[( ),("Harry"),("Stark"),( ),("David"),( ),("oscar"),("parker")]
+List.reverse()
+print(list(emptytuples(List)))
